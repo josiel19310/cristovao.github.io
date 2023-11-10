2422e1c8306a6fd4aae88da24d1f950d68c18a73
mge19310 <josielmartins@outlook.com.br>
2023-11-10 17:05:31 -0300

    adicionando arquivos no git

---
+++ script.js
@@ -0,0 +1,18 @@
+let currentIndex = 0;
+const totalSlides = document.querySelectorAll('.carousel-item').length;
+
+function showSlide(index) {
+    const carousel = document.querySelector('.carousel');
+    const slideWidth = document.querySelector('.carousel-item').clientWidth;
+    carousel.style.transform = `translateX(${-index * slideWidth}px)`;
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    showSlide(currentIndex);
+}
